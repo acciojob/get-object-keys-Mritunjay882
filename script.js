cc1b35e8e16e8f326936c07ccc6bcd222c4e6782
Mritunjay Kumar <101097898+Mritunjay882@users.noreply.github.com>
2023-04-28 18:05:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const student = {
+  name: 'John Smith'
+};
+
+// Add getKeys() method to Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Test getKeys() method
+console.log(student.getKeys()); // Output: ['name']
